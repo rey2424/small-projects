7d0287479f8c771a01d4338766d56511bb949922
rey2424 <61514978+rey2424@users.noreply.github.com>
2020-04-15 08:49:15 +0200

    ES1

---
+++ JS scripts es/people.js
@@ -0,0 +1,42 @@
+export const people = [{
+    id: 1,
+    name: "Hilliard",
+    username: "hzmitrichenko0",
+    email: "hhaylett0@ask.com"
+  }, {
+    id: 2,
+    name: "Alvis",
+    username: "apanniers1",
+    email: "anys1@wp.com"
+  }, {
+    id: 3,
+    name: "Hank",
+    username: "hgault2",
+    email: "hpetti2@gnu.org"
+  }, {
+    id: 4,
+    name: "Tris",
+    username: "tcastaner3",
+    email: "tponton3@webmd.com"
+  }, {
+    id: 5,
+    name: "Bowie",
+    username: "bkinghorn4",
+    email: "bmcandrew4@answers.com"
+  }, {
+    id: 6,
+    name: "Penrod",
+    username: "peveringham5",
+    email: "ppowlesland5@sohu.com"
+  }, {
+    id: 7,
+    name: "Rice",
+    username: "rgiacobillo6",
+    email: "rdarragh6@guardian.co.uk"
+  }, {
+    id: 8,
+    name: "Reinold",
+    username: "rreeders7",
+    email: "rcowlard7@pbs.org"
+  }
+]
